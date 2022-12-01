6b4bf61ea1fd232ba5b39121d99ff08f720d977b
atborg <33327789+atborg@users.noreply.github.com>
2022-12-01 14:19:57 -0700

    code formatting

--- 1/solution.py
+++ 1/solution.py
@@ -1,21 +1,21 @@
-with open('input.txt') as f:
+with open("input.txt") as f:
     lines = f.readlines()
 
 elves = []
 currElfCal = 0
 for line in lines:
-    if line != '\n':
+    if line != "\n":
         currElfCal += int(line)
     else:
         elves.append(currElfCal)
         currElfCal = 0
 
-#part 1 solution
+# part 1 solution
 print(max(elves))
 
 elves.sort()
 
 totalElves = len(elves)
 
-#part 2 solution
-print(sum(elves[totalElves - 3: totalElves]))
+# part 2 solution
+print(sum(elves[totalElves - 3 : totalElves]))
